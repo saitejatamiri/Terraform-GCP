32611f1287fb2e4ec2f5976990feffec95b2e6fd
saitejatamiri <98472448+saitejatamiri@users.noreply.github.com>
2025-08-18 14:56:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12"
+      image = "Ubuntu 25.04 Minimal"
     }
   }
 
